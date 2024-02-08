b9060cf996fd46c078efa79015d36134814f80ef
Lauracandoit <nguyetlt07@gmail.com>
2024-02-08 22:50:37 +0700

    update blogs

--- src/components/Blogs/BlogHome/FeaturedPosts.js
+++ src/components/Blogs/BlogHome/FeaturedPosts.js
@@ -5,8 +5,8 @@ import BlogLayoutTwo from "../Blog/BlogLayoutTwo";
 
 const FeaturedPosts = ({ blogs }) => {
   const sortedBlogs = sortBlogs(blogs);
-  return <section className="w-full sm:mt-24 md:mt-32 px-5 md:px-24 sm:px-10 flex flex-col items-center justify-center">
-    <h2 className="w-full inline-block font-bold capitalize text-2xl md:text-4xl text-dark dark:text-light sm:text-center">Featured Posts</h2>
+  return <section className="w-full sm:mt-24 mt-24 md:mt-32 px-5 md:px-24 sm:px-10 flex flex-col items-center justify-center">
+    <h2 className="w-full inline-block font-bold capitalize text-4xl md:text-4xl text-dark dark:text-light sm:text-center">Featured Posts</h2>
 
     <div className="grid grid-cols-2 lg:grid-cols-1 grid-rows-2 gap-6  mt-10 sm:mt-16 ">
       <article className="col-span-1 w-full h-full lg:col-span-2 row-span-2 relative ">
