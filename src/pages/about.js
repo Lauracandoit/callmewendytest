42f0ebfa7bf42b530d5d0eb473759ce97579ac9e
Lauracandoit <nguyetlt07@gmail.com>
2024-03-25 14:18:39 +0700

    update screen 2pm 25

--- src/pages/about.js
+++ src/pages/about.js
@@ -43,8 +43,8 @@ const about = () => {
 
       <main className="flex w-full flex-col items-center justify-center">
         <Layout className="pt-16">
-          <div className="grid w-full grid-cols-8 gap-16 sm:gap-8  md:grid-cols-8">
-            <div className="col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:!col-span-8 ">
+          <div className="grid w-full grid-cols-8 gap-16">
+            <div className=" col-span-8  flex flex-col items-start justify-start  order-2   py-10 laptop:py-0 laptop:col-span-3 laptop:order-1">
               <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
                 Biography{" "}
               </h2>
@@ -93,7 +93,7 @@ const about = () => {
               </p>
             </div>
 
-            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:!col-span-8  ">
+            <div className="col-span-8 order-1 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light laptop:order-2 laptop:col-span-3  ">
               <div className="absolute top-2 -right-3 -z-10 w-[102%] h-[101%] rounded-[2rem] bg-dark dark:bg-light " />
               <Image
                 src={codinggirl}
@@ -105,7 +105,7 @@ const about = () => {
               />
             </div>
 
-            <div className="col-span-2 flex flex-col items-end justify-between dark:text-light xl:!col-span-8 xl:flex-row xl:items-center md:order-3 sm:flex-row ">
+            <div className="laptop:col-span-2 flex flex-col items-end justify-between dark:text-light tablet:!col-span-8 mobile:!col-span-8 sm:!col-span-8  tablet:flex-row laptop:items-center order-3 sm:flex-row py-10 laptop:py-0">
               <div className="flex flex-col items-end justify-center text-dark dark:text-light xl:items-center ">
                 <span className="inline-block text-7xl font-bold md:text-6xl sm:!text-3xl xs:text-4xl ">
                   <AnimatedNumbers value={100} />+
